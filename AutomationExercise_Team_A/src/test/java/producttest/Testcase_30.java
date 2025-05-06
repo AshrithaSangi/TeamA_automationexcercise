ea0148cf77523ab6f024c7decd08b4c4fcf783cf
jyoth <jyoth@LAPTOP-QPF92E43>
2025-05-06 17:05:26 +0530

    committed by jyothi

---
+++ AutomationExercise_Team_A/src/test/java/producttest/Testcase_30.java
@@ -0,0 +1,11 @@
+package producttest;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Testcase_30 {
+@Test
+public void testcase30() {
+	Reporter.log("testcasr30");
+}
+}
