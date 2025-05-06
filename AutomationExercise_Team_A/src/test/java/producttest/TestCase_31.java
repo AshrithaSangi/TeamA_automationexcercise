d601ce67f24e5b3a3ee0823a91920066b38ff647
91630 <91630@S_sneha1314>
2025-05-06 17:13:32 +0530

    Commited by Sneha

---
+++ AutomationExercise_Team_A/src/test/java/producttest/TestCase_31.java
@@ -0,0 +1,13 @@
+package producttest;
+
+import org.testng.Reporter;
+
+public class TestCase_31 {
+	
+	@org.testng.annotations.Test
+	public void Test()
+	{
+		Reporter.log("Test case 31");
+	}
+
+}
